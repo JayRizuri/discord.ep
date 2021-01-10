2bb9b3d62ac6609adab1baa90eb15a7e6669618e
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 01:47:29 -0500

    Update Base.js

--- src/client/Base.js
+++ src/client/Base.js
@@ -16,5 +16,68 @@ class Base extends eventEmitter {
 		this.options = Utils.mergeDefault(DefaultClientOptions, options);
 		this.rest = new Manager(this);
 	}
+	get api {
+		return this.rest.api;
+	}
+	logout() {
+		for (const t of this._timeouts)
+			this.clearTimeout(t);
+		for (const i of this._intervals)
+			this.clearInterval(i);
+		for (const i of this._immediates)
+			this.clearImmediate(i);
+		this._timeouts.clear();
+		this._intervals.clear();
+		this._immediates.clear();
+	}
+
+
+	setTimeout(fn, delay, ...args) {
+		const timeout = setTimeout(() => {
+			fn(...args);
+			this._timeouts.delete(timeout);
+		}, delay);
+		this._timeouts.add(timeout);
+		return timeout;
+	}
+	clearTimeout(timeout) {
+		clearTimeout(timeout);
+		this._timeouts.delete(timeout);
+	}
+
+	setInterval(fn, delay, ...args) {
+		const interval = setInterval(fn, delay, ...args);
+		this._intervals.add(interval);
+		return interval;
+	}
+	clearInterval(interval) {
+		clearInterval(interval);
+		this._intervals.delete(interval);
+	}
+	setImmediate(fn, ...args) {
+		const immediate = setImmediate(fn, ...args);
+		this._immediates.add(immediate);
+		return immediate;
+	}
+	clearImmediate(immediate) {
+		clearImmediate(immediate);
+		this._immediates.delete(immediate);
+	}
+
+	increaseMaxListeners() {
+		const maxListeners = this.getMaxListeners();
+		if (maxListeners !== 0)
+			this.setMaxListeners(maxListeners + 1);
+	}
+	decreaseMaxListeners() {
+		const maxListeners = this.getMaxListeners();
+		if (maxListeners !== 0)
+			this.setMaxListeners(maxListeners - 1);
+	}
+	json(...props) {
+		return Utils.flatten(this, {
+			domain: false
+		}, ...props);
+	}
 }
 module.exports = Base;
