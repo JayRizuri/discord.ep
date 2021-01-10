d44d4a773e9cbccc6dafb9423758746efa5fcbb4
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-09 23:59:21 -0500

    Update constants.js

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,1 +1,22 @@
-
+exports.DefaultClientOptions = {
+  shardCount: 1,
+  retryLimit: 1,
+  presence: {},
+  ws: {
+    large_threshold: 50,
+    compress: false,
+    properties: {
+      $os: browser ? 'browser' : process.platform,
+      $browser: 'discord.js',
+      $device: 'discord.js',
+    },
+    version: 6,
+  },
+  http: {
+    version: 7,
+    api: 'https://discord.com/api',
+    cdn: 'https://cdn.discordapp.com',
+    invite: 'https://discord.gg',
+    template: 'https://discord.new',
+  },
+};
