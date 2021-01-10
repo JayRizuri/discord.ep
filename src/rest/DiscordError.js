47166b7ddf05acfb2a2b8fac7b29f8235509e3c6
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 00:54:29 -0500

    Create DiscordError.js

---
+++ src/rest/DiscordError.js
@@ -0,0 +1,32 @@
+class DiscordAPIError extends Error {
+	constructor(path, error, method, status) {
+		super();
+		const flattened = this.constructor.flattenErrors(error.errors || error).join('\n');
+		this.name = 'DiscordAPIError';
+		this.message = error.message && flattened ? `${error.message}\n${flattened}` : error.message || flattened;
+		this.method = method;
+		this.path = path;
+		this.code = error.code;
+		this.httpStatus = status;
+	}
+	static flattenErrors(obj, key = '') {
+		let messages = [];
+		for (const [k, v] of Object.entries(obj)) {
+			if (k === 'message')
+				continue;
+			const newKey = key ? (isNaN(k) ? `${key}.${k}` : `${key}[${k}]`) : k;
+		
+			if (v._errors)
+				messages.push(`${newKey}: ${v._errors.map(e => e.message).join(' ')}`);
+			else if (v.code || v.message)
+				messages.push(`${v.code ? `${v.code}: ` : ''}${v.message}`.trim());
+			else if (typeof v === 'string')
+				messages.push(v);
+			else
+				messages = messages.concat(this.flattenErrors(v, newKey));
+		}
+		return messages;
+	}
+}
+
+module.exports = DiscordAPIError;
