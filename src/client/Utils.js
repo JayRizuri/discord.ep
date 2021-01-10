fbd1ae3bcd7c633de723f4e59bf04ce2d0fa93e7
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 01:26:03 -0500

    Update Utils.js

--- src/client/Utils.js
+++ src/client/Utils.js
@@ -1,1 +1,14 @@
-
+const fetch = require('node-fetch'),
+	  has = (o, k) => Object.prototype.hasOwnProperty.call(o, k),
+	  isObject = d => typeof d === 'object' && d !== null;
+class Utils {
+	constructor() {
+		throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
+	}
+	static delayFor(ms) {
+    	return new Promise(resolve => {
+			setTimeout(resolve, ms);
+		});
+	}
+}
+module.exports = Utils;
