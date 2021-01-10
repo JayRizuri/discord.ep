7beb2d2420175e4ff9e500694124f10ebc4914a1
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 00:43:20 -0500

    Update Manager.js

--- src/rest/Manager.js
+++ src/rest/Manager.js
@@ -1,11 +1,19 @@
 const { endpoints } = require('../client/constants'),
-      Request = require('Request')
+      Request = require('Request'),
+      Collection = require('@discordjs/collection');
 class Manager {
 	constructor(client) {
 		this.client = client;
+		this.handlers = new Collection();
 	}
 	request(method, url, options = {}) {
-		const apiRequest = new Request(this, method, url, options);
+		const request = new Request(this, method, url, options);
+		let handler = (this.handlers.has(request.route)) ? this.handlers.get(request.route) : new RequestHandler(this);
+		
+		if (!handler)
+			this.handlers.set(request.route, handler);
+		
+		return handler.push(request);
 	}
 }
 exports.RestManager = Manager;
