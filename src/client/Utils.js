b343bf3958837f0e9fe1b872155691e9984b6476
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 01:51:11 -0500

    Update Utils.js

--- src/client/Utils.js
+++ src/client/Utils.js
@@ -5,8 +5,35 @@ class Utils {
 	constructor() {
 		throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
 	}
-	
-	
+	static flatten(obj, ...props) {
+		if (!isObject(obj))
+			return obj;
+		const objProps = Object.keys(obj)
+			.filter(k => !k.startsWith('_'))
+			.map(k => ({ [k]: true }));
+		props = objProps.length ? Object.assign(...objProps, ...props) : Object.assign({}, ...props);
+		const out = {};
+		
+		for (let [prop, newProp] of Object.entries(props)) {
+			if (!newProp)
+				continue;
+			newProp = newProp === true ? prop : newProp;
+			const element = obj[prop],
+			      elemIsObj = isObject(element),
+			      valueOf = elemIsObj && typeof element.valueOf === 'function' ? element.valueOf() : null;
+			if (element instanceof require('@discord.js/Collection'))
+				out[newProp] = Array.from(element.keys());
+			else if (valueOf instanceof require('./Collection'))
+				out[newProp] = Array.from(valueOf.keys());
+			else if (Array.isArray(element))
+				out[newProp] = element.map(e => Util.flatten(e));
+			else if (typeof valueOf !== 'object')
+				out[newProp] = valueOf;
+			else if (!elemIsObj)
+				out[newProp] = element;
+		}
+		return out;
+	}
 	static mergeDefault(def, given) {
 		if (!given)
 			return def;
