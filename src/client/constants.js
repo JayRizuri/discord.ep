5a415b15599177c2192c2549285abd5b6a7af29e
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 00:06:57 -0500

    Update constants.js

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,3 +1,6 @@
+
+
+
 exports.DefaultClientOptions = {
   shardCount: 1,
   retryLimit: 1,
@@ -19,4 +22,45 @@ exports.DefaultClientOptions = {
     invite: 'https://discord.gg',
     template: 'https://discord.new',
   },
+};
+
+exports.endpoints = {
+	cdn(root) {
+		return {
+			Emoji: ( emojiID, format = 'png' ) => `${root}/emojis/${emojiID}.${format}`,
+      
+			Asset: name => `${root}/assets/${name}`,
+			
+			DefaultAvatar: discriminator => `${root}/embed/avatars/${discriminator}.png`,
+			Avatar: ( userID, hash, format = 'webp', size, dynamic = false ) => {
+				if (dynamic) format = hash.startsWith('a_') ? 'gif' : format;
+				return makeImageUrl(`${root}/avatars/${userID}/${hash}`, { format, size });
+			},
+  
+			Banner: (guildID, hash, format = 'webp', size) =>
+				makeImageUrl(`${root}/banners/${guildID}/${hash}`, { format, size }),
+			Icon: (guildID, hash, format = 'webp', size, dynamic = false) => {
+				if (dynamic) format = hash.startsWith('a_') ? 'gif' : format;
+				return makeImageUrl(`${root}/icons/${guildID}/${hash}`, { format, size });
+			},
+        
+			AppIcon: (clientID, hash, { format = 'webp', size } = {}) =>
+				makeImageUrl(`${root}/app-icons/${clientID}/${hash}`, { size, format }),
+			AppAsset: (clientID, hash, { format = 'webp', size } = {}) =>
+				makeImageUrl(`${root}/app-assets/${clientID}/${hash}`, { size, format }),
+        
+			GDMIcon: (channelID, hash, format = 'webp', size) =>
+				makeImageUrl(`${root}/channel-icons/${channelID}/${hash}`, { size, format }),
+        
+			Splash: (guildID, hash, format = 'webp', size) =>
+				makeImageUrl(`${root}/splashes/${guildID}/${hash}`, { size, format }),
+			DiscoverySplash: (guildID, hash, format = 'webp', size) =>
+				makeImageUrl(`${root}/discovery-splashes/${guildID}/${hash}`, { size, format }),
+        
+			TeamIcon: (teamID, hash, { format = 'webp', size } = {}) =>
+				makeImageUrl(`${root}/team-icons/${teamID}/${hash}`, { size, format }),
+		};
+	},
+	invite: (root, code) => `${root}/${code}`,
+	gateway: '/gateway/bot',
 };
