e6f66e5ec23753fb2b69d65bca56765c9697abe8
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 00:36:17 -0500

    Update Manager.js

--- src/rest/Manager.js
+++ src/rest/Manager.js
@@ -1,7 +1,11 @@
-const { endpoints } = require('../client/constants')
+const { endpoints } = require('../client/constants'),
+      Request = require('Request')
 class Manager {
 	constructor(client) {
 		this.client = client;
+	}
+	request(method, url, options = {}) {
+		const apiRequest = new Request(this, method, url, options);
 	}
 }
 exports.RestManager = Manager;
