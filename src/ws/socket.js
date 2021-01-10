afe712b09db9bfafcc2c3c9439c5ab678cf8fe89
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-09 23:50:25 -0500

    Update socket.js

--- src/ws/socket.js
+++ src/ws/socket.js
@@ -1,2 +1,30 @@
-
 exports.WebSocket = (exports.browser = typeof window !== 'undefined') ? window.WebSocket : require('ws');
+
+exports.create = (gateway, query = {}, ...args) => {
+  const [
+    g,
+    q
+  ] = gateway.split('?');
+  
+  query.encoding = exports.encoding;
+  query = new URLSearchParams(query);
+  
+  if (q) new URLSearchParams(q)
+    .forEach(
+      (v, k) => query.set(k, v));
+  
+  const ws = new exports.WebSocket(`${g}?${query}`, ...args);
+  if (browser) ws.binaryType = 'arraybuffer';
+  return ws;
+};
+
+for (const state of [
+  'CONNECTING',
+  'OPEN',
+  'CLOSING',
+  'CLOSED'])
+  exports[
+    state
+  ] = exports.WebSocket[
+    state
+  ];
