64955000674ff0c8e4b3c00754c6055f6eceb890
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-13 09:27:31 -0500

    Update ModuleHandler.js

--- src/modules/ModuleHandler.js
+++ src/modules/ModuleHandler.js
@@ -14,5 +14,13 @@ class ModuleHandler {
 			throw new ReferenceError(`COuldn't find the module "${moduleName}". Please chack the spelling...`);
 		if (fs.statSync(`${this.options.dir}${moduleName}`).isFIle())
 			throw new TypeError(`The module is not a folder.`);
+		
+		try {
+			let moduleDir = fs.readdirSync(`${this.options.dir}${moduleName}`);
+			
+			if (moduleDir.indexOf("module.json") == -1)
+				throw new ReferenceError(`The module info file could not be found...`);
+			
+		}
 	}
 }
