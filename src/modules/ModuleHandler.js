106e878ad84a99c9d54e9de42d3c2a8340a5bf8d
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-13 09:21:14 -0500

    making it modular

--- src/modules/ModuleHandler.js
+++ src/modules/ModuleHandler.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 
 class ModuleHandler {
-	constructor(client) {
+	constructor(client, options = {}) {
+		let baseOptions = {
+			dir: "./"
+		};
 		this.client = client;
+		this.options = Object.assign(options, baseOptions);
 	}
 	
 	function loadModule(moduleName) {
-		if (!fs.existSync(`./${moduleName}`))
+		if (!fs.existSync(`${this.options.dir}${moduleName}`))
 			throw new ReferenceError(`COuldn't find the module "${moduleName}". Please chack the spelling...`);
-		if (fs.statSync(`./${moduleName}`).isFIle())
+		if (fs.statSync(`${this.options.dir}${moduleName}`).isFIle())
 			throw new TypeError(`The module is not a folder.`);
 	}
 }
