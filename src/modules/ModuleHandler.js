bbe9feaa80709e9dd1094352fb9b5d1516244ed0
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-13 09:47:23 -0500

    Update ModuleHandler.js

--- src/modules/ModuleHandler.js
+++ src/modules/ModuleHandler.js
@@ -1,17 +1,19 @@
-const fs = require("fs");
+const fs = require("fs"),
+      collection = require("../Collection")
 
 class ModuleHandler {
 	constructor(client, options = {}) {
 		let baseOptions = {
-			dir: "./"
+			dir: __dirname
 		};
 		this.client = client;
 		this.options = Object.assign(options, baseOptions);
+		this.modules = new Collection();
 	}
 	
-	function loadModule(moduleName) {
+	loadModule (moduleName) {
 		if (!fs.existSync(`${this.options.dir}${moduleName}`))
-			throw new ReferenceError(`COuldn't find the module "${moduleName}". Please chack the spelling...`);
+			throw new ReferenceError(`Couldn't find the module "${moduleName}". Please chack the spelling...`);
 		if (fs.statSync(`${this.options.dir}${moduleName}`).isFIle())
 			throw new TypeError(`The module is not a folder.`);
 		
@@ -21,6 +23,36 @@ class ModuleHandler {
 			if (moduleDir.indexOf("module.json") == -1)
 				throw new ReferenceError(`The module info file could not be found...`);
 			
+			let moduleData = require(`${this.options.dir}${moduleName}/module.json`),
+			    importedData = {
+				    name: moduleName,
+				    description: "No description has been provided.",
+				    main: null,
+				    version: "0.0.1",
+				    author: "unknown",
+				    directory: `${this.options.dir}${moduleName}`,
+			    }
+			
+			if (Object.keys(moduleData).indexOf("main") == -1)
+				throw new ReferenceError(`The module main file could not be found...`);
+			importedData.main = moduleData.main;
+			
+			if (Object.keys(moduleData).indexOf("description") !== -1)
+				if (moduleData.description.replace(/\s/g, "").length > 0)
+					importedData.description = moduleData.description;
+			
+			if (Object.keys(moduleData).indexOf("name") !== -1)
+				if (moduleData.name.replace(/\s/g, "").length > 0)
+					importedData.name = moduleData.name;
+			
+			if (Object.keys(moduleData).indexOf("author") !== -1)
+				if (moduleData.author.replace(/\s/g, "").length > 0)
+					importedData.author = moduleData.author;
+			
+			if (Object.keys(moduleData).indexOf("version") !== -1)
+				if (moduleData.version.replace(/\s/g, "").length > 0)
+					importedData.version = moduleData.version;
+			this.modules.set(moduleName, importedData)
 		}
 	}
 }
