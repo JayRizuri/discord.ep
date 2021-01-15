02a20061b553e8f93fdbea25a93a66220fb8ffbc
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-15 10:18:54 -0500

    h

--- src/ws/handlers/index.js
+++ src/ws/handlers/index.js
@@ -1,9 +1,9 @@
-const { WSEvents } = require('../../../client/constants'),
-	handlers = {};
+const { WSEvents } = require("../../../constants"),
+  handlers = {};
 
 for (const name of Object.keys(WSEvents))
-	try {
-		handlers[name] = require(`./${name}.js`);
-	} catch {}
+  try {
+    handlers[name] = require(`./${name}.js`);
+  } catch {}
 
 module.exports = handlers;
